4f32716ae21d7455b2b9b2ad9d09730a798b00d2
Hascihan26 <hasancihanbilgin@gmail.com>
2024-08-11 20:42:47 +0300

    Initial Commit

---
+++ Password/PasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordApp.swift
+//  Password
+//
+//  Created by Hasan Cihan Bilgin on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
